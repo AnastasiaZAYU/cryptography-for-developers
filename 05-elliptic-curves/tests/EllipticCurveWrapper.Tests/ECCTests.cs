9350d779c72f84bca5b89d32d000cf830f4c4b36
AnastasiaZAYU <anastasiya7.0312@gmail.com>
2026-02-28 19:37:42 +0200

    test(lab5): verify EllipticCurveWrapper with xUnit

---
+++ 05-elliptic-curves/tests/EllipticCurveWrapper.Tests/ECCTests.cs
@@ -0,0 +1,110 @@
+﻿using System.Numerics;
+using System.Security.Cryptography;
+
+namespace EllipticCurveWrapper.Tests
+{
+    public class ECCTests
+    {
+        private readonly IEllipticCurveService _ecc;
+
+        public ECCTests()
+        {
+            _ecc = new BouncyCastleWrapper("secp256k1");
+        }
+
+        private BigInteger GenerateRandomBigInteger(int bytesCount)
+        {
+            byte[] data = new byte[bytesCount];
+            RandomNumberGenerator.Fill(data);
+            return BigInteger.Abs(new BigInteger(data));
+        }
+
+        [Fact]
+        public void BasePoint_ShouldBeOnCurve()
+        {
+            // Arrange
+            var g = _ecc.BasePointGGet();
+
+            // Act
+            var isOnCurve = _ecc.IsOnCurveCheck(g);
+
+            // Assert
+            Assert.True(isOnCurve, "Base point G must be on the curve.");
+            Assert.NotNull(g.X);
+            Assert.NotNull(g.Y);
+        }
+
+        [Fact]
+        public void ScalarMultiplication_CommutativeProperty_Test()
+        {
+            // k * (d * G) = d * (k * G)
+            // Arrange
+            var g = _ecc.BasePointGGet();
+            var k = GenerateRandomBigInteger(32);
+            var d = GenerateRandomBigInteger(32);
+
+            // Act
+            var dG = _ecc.ScalarMult(d, g);
+            var leftSide = _ecc.ScalarMult(k, dG);
+
+            var kG = _ecc.ScalarMult(k, g);
+            var rightSide = _ecc.ScalarMult(d, kG);
+
+            // Assert
+            Assert.Equal(leftSide, rightSide);
+            Assert.True(_ecc.IsOnCurveCheck(leftSide));
+        }
+
+        [Fact]
+        public void Serialization_ShouldRestoreOriginalPoint()
+        {
+            // Arrange
+            var g = _ecc.BasePointGGet();
+            var k = GenerateRandomBigInteger(32);
+            var originalPoint = _ecc.ScalarMult(k, g);
+
+            // Act
+            var hexString = _ecc.ECPointToString(originalPoint);
+            var restoredPoint = _ecc.StringToECPoint(hexString);
+
+            // Assert
+            Assert.NotNull(restoredPoint.X);
+            Assert.NotNull(restoredPoint.Y);
+            Assert.Equal(originalPoint, restoredPoint);
+            Assert.True(_ecc.IsOnCurveCheck(restoredPoint));
+        }
+
+        [Theory]
+        [InlineData("1")]
+        [InlineData("2")]
+        [InlineData("12345678901234567890")]
+        public void DoublePoint_ShouldEqual_ScalarMultByTwo(string scalarStr)
+        {
+            // P + P = 2 * P
+            // Arrange
+            var g = _ecc.BasePointGGet();
+            var p = _ecc.ScalarMult(BigInteger.Parse(scalarStr) + GenerateRandomBigInteger(8), g);
+
+            // Act
+            var doubled = _ecc.DoubleECPoints(p);
+            var scalarTwo = _ecc.ScalarMult(new BigInteger(2), p);
+
+            // Assert
+            Assert.Equal(doubled, scalarTwo);
+        }
+
+        [Fact]
+        public void InfinityPoint_ShouldBeRecognized()
+        {
+            // Arrange
+            var inf = ECPoint.Infinity;
+
+            // Act
+            var isInf = inf.IsInfinity;
+
+            // Assert
+            Assert.True(isInf);
+            Assert.Null(inf.X);
+        }
+    }
+}
---
+++ 05-elliptic-curves/tests/UnitTest1.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Algebra_on_Elliptic_Curves;
+using Org.BouncyCastle.Math;
+using Org.BouncyCastle.Math.EC;
+
+namespace UnitTestProject
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestECDH()
+        {
+            // S = da * Hb = da * ( db * G ) = db * ( da * G ) = db * Ha
+            var curve = new AlgebraicOperations();
+            var G = curve.BasePointGGet();
+
+            var da = curve.RandomNumber(256);
+            var Ha = curve.ScalarMult(da, G);
+
+            var db = curve.RandomNumber(256);
+            var Hb = curve.ScalarMult(db, G);
+            var Sb = curve.ScalarMult(db, Ha);
+
+            var Sa = curve.ScalarMult(da, Hb);
+
+            Assert.AreEqual(Sa, Sb);
+        }
+
+        [TestMethod]
+        public void TestPointGeneration()
+        {
+            var curve = new AlgebraicOperations();
+            var expected = curve.BasePointGGet();
+
+            var gx = new BigInteger("79BE667EF9DCBBAC55A06295CE870B07029BFCDB2DCE28D959F2815B16F81798", 16);
+            var gy = new BigInteger("483ADA7726A3C4655DA4FBFC0E1108A8FD17B448A68554199C47D08FFB10D4B8", 16);
+            var actual = curve.ECPointGen(gx, gy);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void TestPointMultiplication()
+        {
+            // k * ( d * G ) = d * ( k * G )
+            var curve = new AlgebraicOperations();
+            var G = curve.BasePointGGet();
+            var k = curve.RandomNumber(256);
+            var d = curve.RandomNumber(256);
+
+            var H1 = curve.ScalarMult(d, G);
+            var H2 = curve.ScalarMult(k, H1);
+
+            var H3 = curve.ScalarMult(k, G);
+            var H4 = curve.ScalarMult(d, H3);
+
+            Assert.AreEqual(H2, H4);
+        }
+
+        [TestMethod]
+        public void TestPointDoubling()
+        {
+            // G + G = 2G
+            var curve = new AlgebraicOperations();
+            var G = curve.BasePointGGet();
+
+            var H1 = curve.AddECPoints(G, G);
+
+            var H2 = curve.DoubleECPoints(G);
+
+            Assert.AreEqual(H1, H2);
+        }
+
+        [TestMethod]
+        public void TestAddition()
+        {
+            // ( A + B ) + C = A + ( B + C )
+            var curve = new AlgebraicOperations();
+            var A = curve.RandomPoint();
+            var B = curve.RandomPoint();
+            var C = curve.RandomPoint();
+
+            var H1 = curve.AddECPoints(A, B);
+            var H2 = curve.AddECPoints(H1, C);
+
+            var H3 = curve.AddECPoints(B, C);
+            var H4 = curve.AddECPoints(A, H3);
+
+            Assert.AreEqual(H2, H4);
+        }
+
+        [TestMethod]
+        public void TestPointValid()
+        {
+            // G є CURVE
+            var curve = new AlgebraicOperations();
+            var G = curve.BasePointGGet();
+            var flag = curve.IsOnCurveCheck(G);
+
+            Assert.AreEqual(flag, true);
+        }
+
+        [TestMethod]
+        public void TestTransformation()
+        {
+            var curve = new AlgebraicOperations();
+            var G = curve.BasePointGGet();
+
+            var str = curve.ECPointToString(G);
+
+            var H1 = curve.StringToECPoint(str);
+
+            Assert.AreEqual(G, H1);
+        }
+    }
+}
